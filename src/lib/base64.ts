9e9fd10428fd6bce0c80eb495d9c4eb9b6dfdc0d
Serhii Lytvynenko <lytvynenkosv@gmail.com>
2025-01-05 21:27:01 +0200

    Base64 encode/decode with UTF-8 support

--- src/lib/base64.ts
+++ src/lib/base64.ts
@@ -1,9 +1,13 @@
-import isNodejs from './isNodejs';
-
 export function base64Encode(str: string): string {
-  return isNodejs ? Buffer.from(str).toString('base64') : btoa(str);
+  const encoder = new TextEncoder();
+  const uint8Array = encoder.encode(str);
+  const binaryString = String.fromCharCode(...uint8Array);
+  return btoa(binaryString);
 }
 
 export function base64Decode(str: string): string {
-  return isNodejs ? Buffer.from(str, 'base64').toString() : atob(str);
+  const binaryString = atob(str);
+  const binaryArray = Uint8Array.from(binaryString, (char) => char.charCodeAt(0));
+  const decoder = new TextDecoder();
+  return decoder.decode(binaryArray);
 }
